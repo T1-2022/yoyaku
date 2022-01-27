4601590d3f8c43d0795b6620f94a56ab8a87f29d
hirokishinoda <progac27jhisi@gmail.com>
2022-01-27 06:56:33 +0000

    migrationファイルを追加

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
 
 db = SQLAlchemy()
 
 def init_db(app):
     db.init_app(app)
-    print("sakusei!")
+    Migrate(app, db)
