d061eb9a654c81e0b43cdbabeecabc2625a9aa01
hirokishinoda <progac27jhisi@gmail.com>
2022-01-27 02:07:45 +0000

    起動構成の変更と環境変数ファイルの追加

--- app/app.py
+++ app/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+#from flask import Flask, render_template
+#from project.app.models.database import init_db
+
+# 本番環境に向けてメインファイルの構成を変更
+#app = Flask(__name__)
 
 
-app = Flask(__name__)
+#@app.route("/")
+#def home():
+#    return render_template("index.html")
 
+from flask import Flask
+from app import create_app
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class DevelopmentConfig:
     # SQLAlchemy
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_ECHO = True
 
 
 Config = DevelopmentConfig
