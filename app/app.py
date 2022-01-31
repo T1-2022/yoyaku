d4e009df489d8836296b94da6d1c67cf83528e4c
Osakabe <osakabe.kyosuke.am@tut.jp>
2022-01-31 13:37:14 +0900

    ログイン画面の追加

--- app/app.py
+++ app/app.py
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 from flask import Flask, render_template, redirect
 
 from models import database
 from login import login_bp
+=======
+from flask import Flask, render_template
+from models import database 
+from mainview import main_view
+from changeview import change_view
+>>>>>>> 3929db15e8d64509326a30c69c322dea9a496138
 
 app = Flask(__name__)
 
@@ -11,6 +18,10 @@ app.config.from_object('config.DevelopmentConfig')
 # データベースの初期化
 database.init_db(app)
 app.register_blueprint(login_bp)
+
+# ブルーポイント定義
+app.register_blueprint(main_view)
+app.register_blueprint(change_view)
 
 @app.route("/")
 def home():
