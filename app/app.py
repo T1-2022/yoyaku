af95161dab72272267ed6db17a99042ffa65c97b
Yuto <sato.yuto.kw@tut.jp>
2022-02-01 10:51:35 +0900

    2021/01/28:ログイン画面統合版(sessionの導入)

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect
+from datetime import timedelta
+
+from flask import Flask, render_template, redirect, session
 
 from models import database
 from login import login_bp
@@ -11,6 +13,9 @@ app.config.from_object('config.DevelopmentConfig')
 # データベースの初期化
 database.init_db(app)
 app.register_blueprint(login_bp)
+
+app.secret_key = 'user'
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
--- app/login.py
+++ app/login.py
@@ -1,6 +1,7 @@
 import os
+from datetime import timedelta
 
-from flask import render_template, request, Blueprint, Flask
+from flask import render_template, request, Blueprint, Flask, session
 
 from models.User import User
 from models.database import db
@@ -14,13 +15,14 @@ def login():
 
             attempted_username = request.form['username']
             attempted_password = request.form['password']
-            user = db.session.query(User).filter_by(name=attempted_username).first()
+            user_db = db.session.query(User).filter_by(name=attempted_username).first()
+            session["user"] = attempted_username#セッションでユーザー名を格納
 
-            if user != None and attempted_password == user.__dict__['passwd']:
-                    if user.__dict__['admin'] == 1:
-                        return render_template('admin.html')
+            if user_db != None and attempted_password == user_db.__dict__['passwd']:
+                if user_db.__dict__['admin'] == 1:
+                    return render_template('admin.html')#管理者画面への移行
 
-                    return render_template('main.html')
+                return render_template('main.html')#メイン画面への移行
 
             else:
                 print('invalid credentials')
@@ -30,7 +32,7 @@ def login():
 
 
     except Exception as e:
-        return render_template('error.html')
+        return render_template('error.html')#エラー処理
 
 
 login_bp.secret_key = os.urandom(24)
