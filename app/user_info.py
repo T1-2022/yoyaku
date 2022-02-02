459acdaab432019786046135e756213ddae5fbb4
hirokishinoda <progac27jhisi@gmail.com>
2022-02-02 07:39:39 +0000

    ユーザー情報を変更する仮ページ作成．

--- app/user_info.py
+++ app/user_info.py
@@ -2,6 +2,14 @@ from flask import Blueprint, render_template
 
 user_info_bp = Blueprint('user_info', __name__, url_prefix='/user_info')
 
-@user_info_bp.route('/')
+@user_info_bp.route('/change_name')
 def user_information():
-    return render_template('user_info.html')
+    return render_template('change_user_info/change_user_name.html')
+
+@user_info_bp.route('/change_passwd')
+def user_information():
+    return render_template('change_user_info/change_user_passwd.html')
+
+@user_info_bp.route('/change_mail')
+def user_information():
+    return render_template('change_user_info/change_user_mail.html')
