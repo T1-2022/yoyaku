3022f5c8f85588c7110fc6af7aa6eedeb29526bc
hirokishinoda <progac27jhisi@gmail.com>
2022-02-02 07:41:48 +0000

    ルーティングを修正．

--- app/user_info.py
+++ app/user_info.py
@@ -3,13 +3,13 @@ from flask import Blueprint, render_template
 user_info_bp = Blueprint('user_info', __name__, url_prefix='/user_info')
 
 @user_info_bp.route('/change_name')
-def user_information():
+def change_user_name():
     return render_template('change_user_info/change_user_name.html')
 
 @user_info_bp.route('/change_passwd')
-def user_information():
+def change_user_passwd():
     return render_template('change_user_info/change_user_passwd.html')
 
 @user_info_bp.route('/change_mail')
-def user_information():
+def change_user_mail():
     return render_template('change_user_info/change_user_mail.html')
