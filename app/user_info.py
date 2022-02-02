9fc1174a1b4ea3c47a3e010b5a549e5bb5468174
hirokishinoda <progac27jhisi@gmail.com>
2022-02-02 08:02:39 +0000

    ユーザー情報タブに領域追加．

--- app/user_info.py
+++ app/user_info.py
@@ -1,15 +1,19 @@
 from flask import Blueprint, render_template
 
+# ブル-プリントをインスタンス化
 user_info_bp = Blueprint('user_info', __name__, url_prefix='/user_info')
 
+# ユーザーネーム変更ページを表示
 @user_info_bp.route('/change_name')
 def change_user_name():
     return render_template('change_user_info/change_user_name.html')
 
+# ユーザーパスワード変更ページを表示
 @user_info_bp.route('/change_passwd')
 def change_user_passwd():
     return render_template('change_user_info/change_user_passwd.html')
 
+# ユーザーメールアドレス変更ページを表示
 @user_info_bp.route('/change_mail')
 def change_user_mail():
     return render_template('change_user_info/change_user_mail.html')
