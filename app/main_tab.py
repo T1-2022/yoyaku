342dcd75dc99c809dcb6ad2571dbb27977ed35c1
hirokishinoda <progac27jhisi@gmail.com>
2022-02-03 02:24:47 +0000

    main_tabからユーザー情報を渡して表示．

--- app/main_tab.py
+++ app/main_tab.py
@@ -4,6 +4,7 @@ from flask import request
 from flask import redirect
 from flask import url_for
 from flask import Blueprint
+from flask import session
 from models.database import db
 from models.User import User 
 from models.Reserve import Reserve 
@@ -12,14 +13,13 @@ from models.Conference import Conference
 # ブループリント設定
 main_bp = Blueprint('main_tab', __name__, url_prefix='/main')
 
-
 # メイン画面
 @main_bp.route("/<user_id>", methods=["GET", "POST"])
 def main_tab(user_id):
     # user_idからユーザー名を取得予定
     name=user_id
-    reserves = Reserve.query.all()        
-    return render_template("main_tab.html", name=name, reserves=reserves)
+    reserves = Reserve.query.all()
+    return render_template("main_tab.html", name=name, reserves=reserves, email='tarou@example.com', passwd='passwd')
 
 #ログアウト
 #@main_view.route('/')
