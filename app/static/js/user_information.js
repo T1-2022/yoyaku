43f1065b1e2d6e3835d42ebc87fa9581abf90d2f
hirokishinoda <progac27jhisi@gmail.com>
2022-02-04 09:33:12 +0000

    ログアウト機能を追加

--- app/static/js/user_information.js
+++ app/static/js/user_information.js
@@ -15,14 +15,20 @@ change_user_name_btn.childNodes[1].onclick = function (){
 /* メイルアドレス変更ページへの遷移 */
 let change_user_mail_btn = document.getElementById('change_user_mail_btn');
 // クリックされた際にページ遷移
-change_user_mail_btn.childNodes[1].onclick = function to_change_user_mail_page(){
+change_user_mail_btn.childNodes[1].onclick = function (){
     document.location.href = '/user_info/change_mail';
 };
 
 /* パスワード変更ページへの遷移 */
 let change_user_passwd_btn = document.getElementById('change_user_passwd_btn');
 // クリックされた際にページ遷移
-change_user_passwd_btn.childNodes[1].onclick = function to_change_user_passwd_page(){
+change_user_passwd_btn.childNodes[1].onclick = function (){
     document.location.href = '/user_info/change_passwd';
 };
 
+/* ログアウト機能 */
+let logout_btn = document.getElementById('logout_btn');
+// クリックされた際にログアウト
+logout_btn.childNodes[1].onclick = function (){
+    document.location.href = '/user_info/logout';
+};
