5071dc4919ccf0518be4b096af2cdef1b9f3d0c6
hirokishinoda <progac27jhisi@gmail.com>
2022-02-04 01:56:06 +0000

    ページ遷移をjavascriptに移行．

--- app/static/js/user_information.js
+++ app/static/js/user_information.js
@@ -6,16 +6,23 @@
 */
 
 /* ユーザーネーム変更ページへの遷移 */
-function to_change_user_name_page(){
+let change_user_name_btn = document.getElementById('change_user_name_btn');
+// クリックされた際にページ遷移
+change_user_name_btn.childNodes[1].onclick = function (){
     document.location.href = '/user_info/change_name';
-}
+};
 
 /* メイルアドレス変更ページへの遷移 */
-function to_change_user_mail_page(){
+let change_user_mail_btn = document.getElementById('change_user_mail_btn');
+// クリックされた際にページ遷移
+change_user_mail_btn.childNodes[1].onclick = function to_change_user_mail_page(){
     document.location.href = '/user_info/change_mail';
-}
+};
 
 /* パスワード変更ページへの遷移 */
-function to_change_user_passwd_page(){
+let change_user_passwd_btn = document.getElementById('change_user_passwd_btn');
+// クリックされた際にページ遷移
+change_user_passwd_btn.childNodes[1].onclick = function to_change_user_passwd_page(){
     document.location.href = '/user_info/change_passwd';
-}
+};
+
