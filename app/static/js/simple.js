56b4311edbf746aa0b5117822fa26993df9e3789
hidaiori <hida.iori.yd@tut.jp>
2022-02-10 18:18:48 +0900

    簡易版の修正

--- app/static/js/simple.js
+++ app/static/js/simple.js
@@ -1,6 +1,6 @@
 function coladd() {
         var table = document.getElementById("table");
-        // 行を行末に追加
+        // データを行末に追加
         var row = table.insertRow(-1);
         //td分追加
         var cell1 = row.insertCell(-1);
@@ -8,7 +8,11 @@ function coladd() {
         var cell3 = row.insertCell(-1);
         var cell4 = row.insertCell(-1);
         // セルの内容入力
-        cell1.innerHTML = '行を追加しました';
+        cell1.innerHTML = 'データ日付';
+        cell2.innerHTML = 'データ時間帯';
+        cell3.innerHTML = 'データ会議室';
+        cell4.innerHTML = 'データ利用者';
+
         //cell2.innerHTML = 'この行を削除しますか？<input type="button" value=削除" id="coladd" onclick="coldel(this)">';
     }
     function coldel(obj) {
