139fa0db7542120b988dfbdca4549d4de4b99459
onherrerap <57517440+onherrerap@users.noreply.github.com>
2022-02-17 16:05:31 +0900

    設置機器追加

--- app/admin_room.py
+++ app/admin_room.py
@@ -3,6 +3,7 @@ import os
 from flask import render_template, request, redirect, url_for, Blueprint, Flask, session
 
 from models.Conference import Conference
+from models.Equipment import Equipment
 from models.Register import Register
 from models.User import User
 from models.database import db
@@ -12,4 +13,5 @@ admin_room_bp = Blueprint('admin_room', __name__, url_prefix='/admin_room')
 @admin_room_bp.route('/', methods=["GET", "POST"])
 def admin_room():
     conferences = Conference.query.all()
-    return render_template('admin_room.html', conferences=conferences)
+    equipments = Equipment.query.all()
+    return render_template('admin_room.html', conferences=conferences, equipments=equipments)
